8f9a72fb158f000642333e977e529f96f9fe2854
Josh Kim <kangsan@dhcp-10-102-145-207.wireless.northwestern.private>
2016-11-12 11:22:39 -0600

    firebase initialized js file, no variables yet, not linked to index

---
+++ assets/employee.js
@@ -0,0 +1,14 @@
+var config = {
+    apiKey: "AIzaSyCdPSUK2S4pKJaC_bM-I-ZRZH4PClMyv2Y",
+    authDomain: "cbay-af8ee.firebaseapp.com",
+    databaseURL: "https://cbay-af8ee.firebaseio.com",
+    storageBucket: "cbay-af8ee.appspot.com",
+    messagingSenderId: "352407676079"
+  };
+  firebase.initializeApp(config);
+
+
+// Create a variable to reference the database
+
+var database = firebase.database();
+
